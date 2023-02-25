44cbf86d6f55713e46942be224806e5f43dd2488
lamanpoladli <lamanpoladli@gmail.com>
2023-02-25 17:31:54 +0400

    gg

---
+++ task no3/Program.cs
@@ -0,0 +1,13 @@
+﻿float customer = 1200;
+if (customer < 100) 
+{
+    Console.WriteLine("5%endirim");
+}
+else if (customer >= 100 && customer <= 1000) 
+{
+    Console.WriteLine("8%endirim");
+}
+else
+{
+    Console.WriteLine("10%endirim");
+}
